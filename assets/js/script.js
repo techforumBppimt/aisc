edd5787391ce034a85116bc1530928950ecb9321
Archak Nath <112867859+archakNath@users.noreply.github.com>
2025-02-12 21:16:28 +0530

    content reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ for (let i = 0; i < footer_links.length; i++) {
 }
 
 download_button.onclick = () => {
-    window.open("https://firebasestorage.googleapis.com/v0/b/artco-bb7b5.appspot.com/o/CFP-%20AISC%C2%A02024.pdf?alt=media&token=4a5ae9b0-c296-412c-8310-d01ea0833602", "_blank")
+    window.open("https://firebasestorage.googleapis.com/v0/b/artco-bb7b5.appspot.com/o/CFP%20-%20AISC%202025.pdf?alt=media&token=07b63dbb-1528-4d84-898e-f04c4f68d865", "_blank")
 }
 
 function scrollToSection() {
@@ -46,102 +46,18 @@ function scrollToSection() {
 
 const intSpeakers = [
     {
-        'name': 'Prof. Lau Sian Lun',
-        'position': 'Department of Computing and Information Systems, Sunway University',
+        'name': 'Prof. Kamaljeet Sandhu',
+        'position': 'UNE Business School, Australia',
         'contact': '',
-        'image': '/assets/images/speaker/lau.jpeg',
-        'link': 'https://studyonline.sunway.edu.my/faculty/professor-lau-sian-lun'
+        'image': '/assets/images/speaker/kamal.jpg',
+        'link': 'https://www.une.edu.au/staff-profiles/business/ksandhu'
     },
     {
-        'name': 'Assoc Prof Anupam Chattopadhyay',
-        'position': 'Associate Professor, School of Computer Science and Engineering',
+        'name': 'Prof. Mohamed Nabil Srifi',
+        'position': 'Ibn Tofaïl University, Kenitra',
         'contact': '',
-        'image': '/assets/images/speaker/anupam.jpg',
-        'link': 'https://dr.ntu.edu.sg/cris/rp/rp01076'
-    },
-    {
-        'name': 'Tino Cortesi',
-        'position': 'Full Professor, Università Ca’ Fioscari Venezia',
-        'contact': '',
-        'image': '/assets/images/speaker/tino.jpg',
-        'link': 'https://www.unive.it/data/people/5591776'
-    },
-    {
-        'name': 'Kenneth W. Regan',
-        'position': 'Professor, University at Buffalo',
-        'contact': '',
-        'image': '/assets/images/speaker/regan.jpg',
-        'link': 'https://cse.buffalo.edu/~regan/'
-    },
-    {
-        'name': 'Dr. Partha Pratim Das (PPD)',
-        'position': 'Professor, Dept. of CSE, IIT Kharagpur',
-        'contact': '',
-        'image': '/assets/images/speaker/ppd.png',
-        'link': 'https://www.linkedin.com/in/ppdas/'
-    },
-    {
-        'name': 'Bimal Roy',
-        'position': 'ISI, Kolkata',
-        'contact': '',
-        'image': '/assets/images/speaker/bimal.jpg',
-        'link': 'https://www.isical.ac.in/~bimal/'
-    },
-    {
-        'name': 'Dr. Nandini Mukherjee',
-        'position': 'Professor, Jadavpur University',
-        'contact': '',
-        'image': '/assets/images/speaker/nandini.jpg',
-        'link': 'http://www.jaduniv.edu.in/profile.php?uid=875'
-    },
-    {
-        'name': 'Aditya Kr Sinha',
-        'position': 'Director at C-DAC Patna & C-DAC Kolkata',
-        'contact': '',
-        'image': '/assets/images/speaker/sinha.jpeg',
-        'link': 'https://www.linkedin.com/in/adityaksinha/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
-    },
-    {
-        'name': 'Shri Sanjay Kumar Das',
-        'position': 'W.B.C.S (Exe.), Managing Director',
-        'contact': '',
-        'image': '/assets/images/speaker/sanjay.jpeg',
-        'link': 'https://www.linkedin.com/in/sanjaykumardas/?originalSubdomain=in'
-    },
-    {
-        'name': 'Ms. Kamiya Khatter',
-        'position': 'Associate Editor, Applied Sciences and Engineering, Springer Nature India',
-        'contact': '',
-        'image': '/assets/images/speaker/kamiya.jpg',
-        'link': 'https://www.springer.com/it/authors-editors/suvira-srivastav/16744110'
-    },
-    {
-        'name': 'Sourabh Mukherjee',
-        'position': 'Lead - Modern Data Architecture, Accenture',
-        'contact': '',
-        'image': '/assets/images/speaker/sourabh.jpeg',
-        'link': 'https://www.linkedin.com/in/sourabhmukherjee/'
-    },
-    {
-        'name': 'Dr. Saptarsi Goswami',
-        'position': 'Bangabasi Morning College',
-        'contact': '',
-        'image': '/assets/images/speaker/saptashri.jpeg',
-        'link': 'https://scholar.google.com/citations?user=kdlCB6sAAAAJ&hl=enc:\Users\archa\Downloads\saptashri.jpeg'
-    },
-    {
-        'name': 'Arijit Mukherjee',
-        'position': 'Principal Scientist, TCS Research',
-        'contact': '',
-        'image': '/assets/images/speaker/arijit.jpg',
-        'link': 'https://scholar.google.co.in/citations?user=99_nlsMAAAAJ&hl=en'
-    },
-    {
-        'name': 'Jibendu Mazumder',
-        'position': 'Director, Cognizant Technology Solutions',
-        'contact': '',
-        'image': '/assets/images/speaker/jivyendu.jpg',
-        'link': 'https://www.linkedin.com/in/jibendu-mazumder-78237a5/'
+        'image': '/assets/images/speaker/nabil.jpg',
+        'link': 'https://www.linkedin.com/in/m-nabil-srifi-b712658a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
     },
 ]
 
@@ -203,7 +119,7 @@ tutorialBtn.onclick = () => {
 }
 const listernerBtn = document.getElementById('listenerBtn');
 listernerBtn.onclick = () => {
-    window.open('https://docs.google.com/forms/d/e/1FAIpQLScrElN43XbtMshUQFDQ3tk08_wsAuLXRJxx5q6E0emr6mW3yw/viewform?usp=pp_url', '_blank')
+    alert('Registration link coming soon');
 }
 const posterButton = document.getElementById('posterButton');
 posterButton.onclick = () => {
